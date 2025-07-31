eb9d025dd14ee1364aabdeb1fefd898847056cfd
Leveridique <habibtelecom224@yahoo.com>
2025-07-31 11:53:28 +0200

    modification du fichier viewconnexxion dans la branche habib

---
+++ App/View/ViewConnexion.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html><html lang="fr">
+<head>
+      <meta charset="UTF-8">
+     <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+     <script src="./public/assets/scripts/main.js"async><script>
+>    <title>Accueil</title>
+> </head>
+> <body>
+>     <h1> Bienvenue sur notre super site </h1>
+> </body>
+> </hml>
+cat <<EOF>> App/View/ViewConnexion.php
+<!DOCTYPE html><html lang="fr">
+<head>
+      <meta charset="UTF-8">
+     <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+     <script src="./public/assets/scripts/main.js"async><script>
+>    <title>Accueil</title>
+> </head>
+> <body>
+>     <h1> Se connextter </h1>
+    <form action="" method="post">
+    <label for="email" name="email">
+cat <<EOF>> App/View/ViewConnexion.php
+<!DOCTYPE html><html lang="fr">
+<head>
+      <meta charset="UTF-8">
+     <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+     <script src="./public/assets/scripts/main.js"async><script>
+>    <title>Accueil</title>
+> </head>
+> <body>
+>     <h1> Bienvenue sur notre super site </h1>
+> </body>
+> </hml>
+cat <<EOF>> App/View/ViewConnexion.php
+<!DOCTYPE html><html lang="fr">
+<head>
+      <meta charset="UTF-8">
+     <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+     <script src="./public/assets/scripts/main.js"async><script>
+>    <title>Accueil</title>
+> </head>
+> <body>
+>     <h1> Se connextter </h1>
+    <form action="" method="post">
+    <label for="email"> Veuillez saisisir votre adresse email : </label>
+  <input type="email" name="email">
+  <label for="password">Veuillez saisiir votre mot de passe</label>
+  <input type="password" name="password">
+  <input type="submit" name:"submit" value="connexion">
+  </form>
+</body>
+</html>
